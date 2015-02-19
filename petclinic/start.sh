b320a0777766aaefaaf5ac9dcf41854b5ac54fda
Valery Shorin <valery.shorin@smddev.com>
2015-02-19 18:07:19 +0300

    step 2. tomcat configured for mysql (not works)

--- petclinic/start.sh
+++ petclinic/start.sh
@@ -1,5 +1,8 @@
 sudo docker run	\
 	-d \
 	-v $HOME/petclinic/petclinic.war:/usr/local/tomcat/webapps/petclinic.war:ro \
+    -v $HOME/petclinic/data-access.properties:/usr/local/tomcat/webapps/petclinic/WEB-INF/classes/spring/data-access.properties:ro \
+    -v $HOME/petclinic/mysql-connector-java-5.1.34-bin.jar:/usr/local/tomcat/webapps/petclinic/WEB-INF/lib/mysql-connector.jar:ro \
 	-p 8080:8080 \
+	--link mysql-petclinic:mysql
 	tomcat:7.0
